b0378317c86eafc5485743f7152afc0eba55c97b
ajiajith426 <ajith.m@capecomsolutions.com>
2022-04-21 16:13:12 +0530

    user changes...

--- src/app/views/app/users/index.jsx
+++ src/app/views/app/users/index.jsx
@@ -11,6 +11,7 @@ import SelectField from '../../../../_metronic/_partials/controls/forms/SelectFi
 const initialValues = {
     username: '',
     email: '',
+    phone: '',
     confirm_password: '',
     role: ''
 }
@@ -18,6 +19,7 @@ const initialValues = {
 const schema = yup.object().shape({
     username: yup.string().required(),
     email: yup.string().email().required(),
+    phone: yup.number().required(),
     confirm_password: yup.string().required(),
     role: yup.string().required(),
 });
@@ -31,7 +33,7 @@ export default function Users() {
         <>
             <Card className="reportfilter-Ctr">
                 <Card.Header className="pb-3">
-                    <h5>Change Password</h5>
+                    <h5>Add User</h5>
                 </Card.Header>
                 <Card.Body className="pt-4 pb-4">
                     <Formik
@@ -44,69 +46,73 @@ export default function Users() {
                         }}
                     >
                         {({ values, setFieldValue, handleBlur }) => (
-                            <Form>
-                                <Row gutter={[20, 20]}>
-
-                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                        <Field
-                                            name="username"
-                                            component={Input}
-                                            placeholder="Enter user name"
-                                            label="Name"
-                                            type="text"
-                                        />
-                                    </Col>
-
-                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                        <Field
-                                            name="email"
-                                            component={Input}
-                                            placeholder="Enter Email ID"
-                                            label="Email"
-                                            type="email"
-                                        />
-                                    </Col>
-
-                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                        <Field
-                                            name="phone"
-                                            component={Input}
-                                            placeholder="Enter phone number"
-                                            label="Phone Number"
-                                            type="number"
-                                        />
-                                    </Col>
-
-                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                        <Field
-                                            name="password"
-                                            component={Input}
-                                            placeholder="Enter password"
-                                            label="Password"
-                                            type="password"
-                                        />
-                                    </Col>
-
-                                    <Col xs={12} sm={12} md={12} lg={12} xl={12}>
-                                        <Field
-                                            component={SelectField}
-                                            name="role" label="Role"
-                                            placeholder="Select Role"
-                                            option={roles}
-                                            values={values}
-                                            setFieldValue={setFieldValue}
-                                            handleBlur={handleBlur}
-                                        />
-                                    </Col>
-
-                                </Row>
-
-                                <div className="text-left">
-                                    <Button type="submit">
-                                        Save
-                                    </Button>
-                                </div>
-                            </Form>
+                            <div className="userform-Ctr">
+                                <Form>
+                                    <Row gutter={[20, 20]}>
+                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                            <Field
+                                                name="username"
+                                                component={Input}
+                                                placeholder="Enter user name"
+                                                label="Name"
+                                                type="text"
+                                            />
+                                        </Col>
+                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                            <Field
+                                                name="email"
+                                                component={Input}
+                                                placeholder="Enter Email ID"
+                                                label="Email"
+                                                type="email"
+                                            />
+                                        </Col>
+                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                            <Field
+                                                name="phone"
+                                                component={Input}
+                                                placeholder="Enter phone number"
+                                                label="Phone Number"
+                                                type="number"
+                                            />
+                                        </Col>
+                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                            <Field
+                                                name="password"
+                                                component={Input}
+                                                placeholder="Enter password"
+                                                label="Password"
+                                                type="password"
+                                            />
+                                        </Col>
+                                        <Col xs={12} sm={12} md={12} lg={12} xl={12}>
+                                            <Field
+                                                component={SelectField}
+                                                name="role" label="Role"
+                                                placeholder="Select Role"
+                                                option={roles}
+                                                values={values}
+                                                setFieldValue={setFieldValue}
+                                                handleBlur={handleBlur}
+                                            />
+                                        </Col>
+                                    </Row>
+                                    <div className="text-left">
+                                        <Row>
+                                            <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+                                                <Button type="submit" className="w-100">
+                                                    Save
+                                                </Button>
+                                            </Col>
+                                            <Col xs={6} sm={6} md={6} lg={6} xl={6}>
+                                                <Button className="w-100">
+                                                    Reset
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </div>
+                                </Form>
+                            </div>
                         )}
                     </Formik>
                 </Card.Body>
